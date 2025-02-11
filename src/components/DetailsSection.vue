582c84699d118b9dea968d499b207ea27a1e8823
mantis <morauszki.andras@gmail.com>
2025-02-11 11:57:23 +0100

    Button styles

--- src/components/DetailsSection.vue
+++ src/components/DetailsSection.vue
@@ -37,4 +37,29 @@ h2 {
   font-size: 1.125rem;
   color: #303b59;
 }
+
+button {
+  --start: #303b59;
+  --end: #303b59;
+  background: linear-gradient(var(--start), var(--end));
+  color: #fff;
+  font-size: 1.125rem;
+  font-weight: 700;
+  padding: 1rem 0;
+  border-radius: 100px;
+  cursor: pointer;
+  border: none;
+  transition: all 0.3s;
+}
+
+button:hover,
+button:focus {
+  --start: #6943ff;
+  --end: #2f2ce9;
+  outline: 0;
+}
+
+button:active {
+  transform: scale(0.95);
+}
 </style>
