1c4cd730051f01c6f7b7e6939717161254f873c7
mantis <morauszki.andras@gmail.com>
2025-02-11 11:22:44 +0100

    Add Result Item component styling

--- src/components/ResultItem.vue
+++ src/components/ResultItem.vue
@@ -10,15 +10,79 @@ export default {
       return new URL(path, import.meta.url).href;
     },
   },
+  computed: {
+    class() {
+      return this.category.toLowerCase();
+    },
+  },
 };
 </script>
 
 <template>
-  <li>
-    <span>
+  <li :class="class">
+    <span class="category">
       <img :src="getImageUrl(icon)" :alt="`${category} icon`" />
       {{ category }}
     </span>
-    <span> {{ score }} / 100 </span>
+    <span class="score"
+      ><strong>{{ score }}</strong> / 100
+    </span>
   </li>
 </template>
+
+<style scoped>
+li {
+  --reaction: hsl(0, 100%, 67%);
+  --reaction-bg: hsla(0, 100%, 67%, 5%);
+  --memory: hsl(39, 100%, 56%);
+  --memory-bg: hsla(39, 100%, 56%, 5%);
+  --verbal: hsl(166, 100%, 37%);
+  --verbal-bg: hsla(166, 100%, 37%, 5%);
+  --visual: hsl(234, 85%, 45%);
+  --visual-bg: hsla(234, 85%, 45%, 5%);
+  padding: 1rem;
+  border-radius: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.category {
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+}
+
+.score {
+  color: #303b59;
+  word-spacing: 5px;
+}
+
+.reaction {
+  background-color: var(--reaction-bg);
+  .category {
+    color: var(--reaction);
+  }
+}
+
+.memory {
+  background-color: var(--memory-bg);
+  .category {
+    color: var(--memory);
+  }
+}
+
+.verbal {
+  background-color: var(--verbal-bg);
+  .category {
+    color: var(--verbal);
+  }
+}
+
+.visual {
+  background-color: var(--visual-bg);
+  .category {
+    color: var(--visual);
+  }
+}
+</style>
