6069244f1272ec3c572490f54b424696db7df5d6
mantis <morauszki.andras@gmail.com>
2025-02-13 22:30:12 +0100

    Add repo name to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/fm-results-summary-component/",
   plugins: [vue()],
-})
+  resolve: {
+    alias: [],
+  },
+});
